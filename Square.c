cc19328840202ecfdf47a0557622593124d7a5c0
Gaetano Maurizio Abbaticchio <91220051+ItsGaet@users.noreply.github.com>
2023-10-13 10:31:57 +0200

    Update Square.c

--- Square.c
+++ Square.c
@@ -2,19 +2,20 @@
 
 int main() {
   // Stampa Strutturata
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+  int n=5;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             printf("+---");
         }
         printf("+\n");
 
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < n; j++) {
             printf("|   ");
         }
         printf("|\n");
     }
     
-    for (int j = 0; j < 5; j++) {
+    for (int j = 0; j < n; j++) {
         printf("+---");
     }
     printf("+\n");
